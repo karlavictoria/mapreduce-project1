1aac6f0b4ff5299eaede26fe79d57c77eaf3c1ed
karlavictoria <46350622+karlavictoria@users.noreply.github.com>
2020-03-20 13:51:16 -0400

    update

--- part2/2a/round1/mapper.py
+++ part2/2a/round1/mapper.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/python 
 import sys
 
 for line in sys.stdin:
@@ -8,6 +7,13 @@ for line in sys.stdin:
         playerA = line[19].title()
         playerB = (line[14].split()[1] + ' ' +  line[14].split()[0])
         shotresult = line[13]
-        print "%s\t%s" % (playerA + ',' + playerB + ',' + shotresult,1)
+        
     except IndexError:
-        continue
+        if line[13] not in ['made','missed']:
+                continue
+        else:
+            
+            playerB = line[14]
+            shotresult = line[13]
+
+    print "%s\t%s" % (playerA + ',' + playerB + ',' + shotresult,1)
